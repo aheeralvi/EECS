680c7576d775e3d5ae9da4f155f8f106dbfd5a0a
Aheer Alvi <aheeralvi@aheers-macbook-pro.local>
2017-09-07 16:46:10 -0400

    Initial Commit

---
+++ EECS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EECS
+//
+//  Created by Aheer Alvi on 9/7/17.
+//  Copyright © 2017 UMICH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
